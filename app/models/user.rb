2fc216c43399eb703f0216a769861f274bfff580
Jozef Harinek <j.harinek@gmail.com>
2014-03-10 20:27:58 +0100

    Add user controller and model

---
+++ app/models/user.rb
@@ -0,0 +1,48 @@
+class User < ActiveRecord::Base
+
+  devise :database_authenticatable,
+         :confirmable,
+         :lockable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+
+         authentication_keys: [:login]
+
+  validates :login, format: { with: /\A[A-Za-z0-9]+\z/ }, presence: true, uniqueness: { case_sensitive: false }
+  validates :nick,  format: { with: /\A[A-Za-z0-9]+\z/ }, presence: true, uniqueness: { case_sensitive: false }
+
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }, presence: true, uniqueness: { case_sensitive: false }
+
+  validates :first, format: { with: /\A\p{Lu}\p{Ll}*\z/u }, allow_blank: true
+  validates :last,  format: { with: /\A\p{Lu}\p{Ll}*\z/u }, allow_blank: true
+
+  def login=(value)
+    write_attribute :login, value,to_s.downcase
+
+    self.nick ||= login
+  end
+
+  def name
+    (value = "#{first} #{last}".squeeze(' ').strip).blank? ? nil : value
+  end
+
+  def self.create_without_confirmation!(attributes)
+    user = User.new(attributes)
+
+    user.skip_confirmation!
+    user.save!
+    user
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    login      = conditions.delete(:login)
+
+    return where(conditions).first unless login
+
+    where(conditions).where(["login = :value OR email = :value", { value: login.downcase }]).first
+  end
+end
