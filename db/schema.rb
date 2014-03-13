5c9f1836953c869f343ab4db8f983aa619527a44
Jozef Harinek <j.harinek@gmail.com>
2014-03-13 01:27:04 +0100

    Update schema and run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140310181440) do
+ActiveRecord::Schema.define(version: 20140313000316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: true do |t|
+    t.integer  "sentence_id",      null: false
+    t.integer  "exercise_id",      null: false
+    t.json     "teacher_solution"
+    t.json     "student_solution"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "login",                               null: false
