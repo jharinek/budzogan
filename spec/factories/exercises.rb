06a6c6fdef15f5c07c1723a27239562be1a9f7e2
Jozef Harinek <j.harinek@gmail.com>
2014-03-16 17:18:23 +0100

    Add spec templates for models

---
+++ spec/factories/exercises.rb
@@ -0,0 +1,11 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :exercise do
+    template_id 1
+    group_id 1
+    sentence_length 1
+    sentence_difficulty "MyString"
+    sentence_source "MyString"
+  end
+end
