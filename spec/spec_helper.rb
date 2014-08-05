2ecc14db8d579c399bf9dd8ef7a72f1a1144ce0a
Jozef Harinek <j.harinek@gmail.com>
2014-08-05 17:49:05 +0200

    Update rspec setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,18 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+
+# Capybara
+require 'capybara/rspec'
+require 'capybara/rails'
+require 'capybara/poltergeist'
+
+# Cancan
+# require 'cancan/matchers'
+
+Capybara.default_selector  = :css
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -27,7 +37,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
@@ -39,4 +49,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # FactoryGirl
+  config.include FactoryGirl::Syntax::Methods
 end
