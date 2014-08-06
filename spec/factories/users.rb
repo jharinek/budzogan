cfa8ffe29315c5b7c2bc274598b3c00094912481
Jozef Harinek <j.harinek@gmail.com>
2014-08-06 16:22:45 +0200

    Update user and workgroup factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,26 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :user do
+  factory :user, aliases: [:student] do
+    sequence(:login) { |n| "user_#{n}" }
+    sequence(:nick)  { |n| "nick_#{n}" }
+    sequence(:email) { |n| "user_#{n}@mailer.com" }
+
+    password 'password'
+    password_confirmation 'password'
+
+    first 'John'
+    last  'Doe'
+
+    # TODO change according to roles model
+    role :student
+
+    factory :teacher, class: User do
+      role :teacher
+    end
+
+    after :create do |user|
+      user.confirm!
+    end
   end
 end
--- spec/factories/workgroups.rb
+++ spec/factories/workgroups.rb
@@ -2,6 +2,17 @@
 
 FactoryGirl.define do
   factory :workgroup do
-    name "MyString"
+    sequence(:name) { |n| "Workgroup ##{n}" }
+
+    association :teacher
+
+    trait :with_students do
+      after :create do |workgroup|
+        3.times do
+          student = create :student
+          Enrollment.create(workgroup_id: workgroup.id, student_id: student.id)
+        end
+      end
+    end
   end
 end
