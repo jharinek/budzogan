459b6689a4b07344fd7c3e1ddf066dd3a9755c8a
Jozef Harinek <j.harinek@gmail.com>
2014-08-15 15:23:59 +0200

    Add update action to build exercise controller

--- app/controllers/exercises/build_controller.rb
+++ app/controllers/exercises/build_controller.rb
@@ -4,8 +4,19 @@ class Exercises::BuildController < ApplicationController
   steps :setup, :sentences, :assignment
 
   def show
-    # @exercise = Exercise.find(params[:exercise_id])
-
+    @exercise = Exercise.find(params[:exercise_id])
     render_wizard
+  end
+
+  def update
+    @exercise = Exercise.find(params[:exercise_id])
+    @exercise.update(exercise_params)
+
+    render_wizard @exercise
+  end
+
+  private
+  def exercise_params
+    params.require(:exercise).permit(:description, :sentence_length, :sentence_difficulty, :sentence_source, :state)
   end
 end
