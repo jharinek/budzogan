0cded1a6cb2951c5446c4d10ac843b67175529ae
Jozef Harinek <j.harinek@gmail.com>
2014-08-15 11:24:35 +0200

    Update routes for building exercise

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Budzogan::Application.routes.draw do
 
   resources :workgroups
 
-  resources :exercises, only: [:new, :create, :edit, :index]
-
+  resources :exercises, only: [:new, :create, :edit, :index] do
+    resources :build, controller: 'exercises/build'
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
