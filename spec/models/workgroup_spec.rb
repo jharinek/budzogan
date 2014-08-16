a58f0524d79305f5f6cda6f5cdc195464755f599
Jozef Harinek <j.harinek@gmail.com>
2014-08-16 17:32:38 +0200

    Add specs for workgroup

--- spec/models/workgroup_spec.rb
+++ spec/models/workgroup_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Workgroup do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'requires correct name' do
+    workgroup = build :workgroup, name:''
+    expect(workgroup).not_to be_valid
+
+    workgroup = build :workgroup, name: '?weird_chars!;,*'
+    expect(workgroup).not_to be_valid
+
+    workgroup = build :workgroup, name: '8.A class'
+    expect(workgroup).to be_valid
+  end
 end
