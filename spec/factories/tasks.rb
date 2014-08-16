0917cd48e2d4c09c7b0ccd5615ccfb0ebc7b8536
Jozef Harinek <j.harinek@gmail.com>
2014-08-16 17:29:44 +0200

    Add factory for tasks

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,10 +1,21 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :task do
-    sentence_id 1
-    exercise_id 1
-    teacher_solution ""
-    student_solution ""
+    association :exercise
+
+    state 0
+
+    trait :with_sentence do
+      after :create do |task|
+        create :sentence, task: task
+      end
+    end
+
+    trait :in_progress do
+      state 1
+    end
+
+    trait :solved do
+      state 2
+    end
   end
 end
