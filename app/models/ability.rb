1f2a80128c16b323470b66b31acfc4fcb1eca45a
Jozef Harinek <j.harinek@gmail.com>
2014-08-19 14:49:16 +0200

    Add abilities for teacher

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,13 @@ class Ability
 
   def initialize(user)
     user ||= User.new
-    
+
+    if user.role? :student
+    end
+
+    if user.role? :teacher
+      can :administrate, Exercise
+      can :administrate, Workgroup
+    end
   end
 end
