824edfc968cee5b8b54dec3260bcc36e7858a67d
Jozef Harinek <j.harinek@gmail.com>
2014-09-05 12:33:59 +0200

    Rename tags spec files

--- spec/factories/attributes.rb
+++ spec/factories/attributes.rb
@@ -1,7 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :tag, :class => 'Tag' do
+  factory :attribute, :class => 'Attribute' do
     name "MyString"
   end
 end
