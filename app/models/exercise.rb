e788c0ef1f37a10470f8c3eaf31a191ae4a85220
Jozef Harinek <j.harinek@gmail.com>
2014-09-10 12:12:51 +0200

    Add validations to Exercise and refactor status methods

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,4 +1,7 @@
 class Exercise < ActiveRecord::Base
+  STATUSES = [:new, :setup, :sentences, :assignment, :active]
+  LEVELS   = [:easy, :medium, :hard]
+
   belongs_to :template, class_name: :ExerciseTemplate
   belongs_to :workgroup
 
@@ -7,7 +10,28 @@ class Exercise < ActiveRecord::Base
 
   has_many :tasks, dependent: :destroy
 
+  validates :description, format: { with: // }, presence: true
+  validates :sentence_length, numericality: { only_integer: true, greater_than: 0 }, presence: true
+
+  validates :sentence_difficulty, presence: true
+  symbolize :sentence_difficulty, in: LEVELS
+
+  validates :status, presence: true
+  symbolize :status, in: STATUSES
+
   def active?
-    state == 'active'
+    status == :active
+  end
+
+  def active_or_setup?
+    status.eql?(:setup) || active?
+  end
+
+  def active_or_sentences?
+    status.eql?(:sentences) || active?
+  end
+
+  def active_or_assignment?
+    status.eql?(:assignment) || active?
   end
 end
