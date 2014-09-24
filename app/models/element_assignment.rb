84a45fa3a09d61e38c1335734017cf762e20653b
Jozef Harinek <j.harinek@gmail.com>
2014-09-24 11:36:54 +0200

    Add validation for unique index

--- app/models/element_assignment.rb
+++ app/models/element_assignment.rb
@@ -1,4 +1,6 @@
 class ElementAssignment < ActiveRecord::Base
   belongs_to :element
   belongs_to :elementable, polymorphic: true
+
+  validates_uniqueness_of :element_id, scope: [:elementable_id]
 end
