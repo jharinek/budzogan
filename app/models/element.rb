30eaed6f2d5066778785f489d866a20f93684822
Jozef Harinek <j.harinek@gmail.com>
2015-03-06 12:54:29 +0100

    Add scopes for different element types

--- app/models/element.rb
+++ app/models/element.rb
@@ -1,6 +1,9 @@
 class Element < ActiveRecord::Base
   has_many :element_assignments, dependent: :destroy
 
+  scope :elements,  lambda { where(category: 'sentence_element') }
+  scope :relations, lambda { where(category: 'elements_relation') }
+
   with_options through: :element_assignments, source: :elementable do |element|
     element.has_many :templates, class_name: :ExerciseTemplate, source_type: 'ExerciseTemplate'
     element.has_many :exercises, source_type: 'Exercise'
