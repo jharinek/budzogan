08925d94a54c30f8517fd3af187981eb264225e4
Jozef Harinek <j.harinek@gmail.com>
2015-04-05 12:35:22 +0200

    Compute sentece length before create

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -3,7 +3,20 @@ class Sentence < ActiveRecord::Base
 
   has_many :tasks
 
+  scope :needed, lambda{ where(tasks.count < 6) }
+
+  scope :long,   lambda{ |count| needed.where('length > 15').limit(count) }
+  scope :medium, lambda{ |count| needed.where('length > 5').limit(count) }
+  scope :short,  lambda{ |count| needed.where('length < 6').limit(count) }
+
+  before_create :compute_length
+
   def self.get_bulk(size)
-    limit(size)
+    self.limit(size)
+  end
+
+  private
+  def compute_length
+    length = content.split(' ').count
   end
 end
