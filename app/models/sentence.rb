0b651016eb0eb2c6879c88a7a7df563b0f32e931
Jozef Harinek <j.harinek@gmail.com>
2015-04-05 12:51:23 +0200

    Refactor automatic sentence generating

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -3,16 +3,25 @@ class Sentence < ActiveRecord::Base
 
   has_many :tasks
 
-  scope :needed, lambda{ where(tasks.count < 6) }
+  scope :needed, lambda{ where('tasks_count < 6') }
 
-  scope :long,   lambda{ |count| needed.where('length > 15').limit(count) }
-  scope :medium, lambda{ |count| needed.where('length > 5').limit(count) }
+  scope :long,   lambda{ |count| needed.where('length > 10').limit(count) }
+  scope :medium, lambda{ |count| needed.where('length > 5').where('length < 11').limit(count) }
   scope :short,  lambda{ |count| needed.where('length < 6').limit(count) }
 
   before_create :compute_length
 
   def self.get_bulk(size)
-    self.limit(size)
+    remaining = size
+    bulk_size = size/3
+
+    sentences = self.long(bulk_size)
+    remaining -= bulk_size
+    self.medium(bulk_size).each { |s| sentences << s }
+    remaining -= bulk_size
+    self.short(remaining).each { |s| sentences << s }
+
+    sentences
   end
 
   private
