6b8ece2b825bed95408941998f89f573b184a3c0
Jozef Harinek <j.harinek@gmail.com>
2015-04-06 16:57:01 +0200

    Do not use sentences that were already used for user

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -3,23 +3,23 @@ class Sentence < ActiveRecord::Base
 
   has_many :tasks
 
-  scope :needed, lambda{ where('tasks_count < 6') }
+  scope :needed, lambda{ |ids_list| where('tasks_count < 6').where('id not in (?)', ids_list) }
 
-  scope :long,   lambda{ |count| needed.where('length > 10').limit(count) }
-  scope :medium, lambda{ |count| needed.where('length > 5').where('length < 11').limit(count) }
-  scope :short,  lambda{ |count| needed.where('length < 6').limit(count) }
+  scope :long,   lambda{ |count, user| needed(invalid_ids_for(user)).where('length > 10').limit(count) }
+  scope :medium, lambda{ |count, user| needed(invalid_ids_for(user)).where('length > 5').where('length < 11').limit(count) }
+  scope :short,  lambda{ |count, user| needed(invalid_ids_for(user)).where('length < 6').limit(count) }
 
   before_create :compute_length
 
-  def self.get_bulk(size)
+  def self.get_bulk(size, assignee)
     remaining = size
     bulk_size = size/3
 
-    sentences = self.long(bulk_size)
+    sentences = self.long(bulk_size, assignee)
     remaining -= bulk_size
-    self.medium(bulk_size).each { |s| sentences << s }
+    self.medium(bulk_size, assignee).each { |s| sentences << s }
     remaining -= bulk_size
-    self.short(remaining).each { |s| sentences << s }
+    self.short(remaining, assignee).each { |s| sentences << s }
 
     sentences
   end
@@ -27,5 +27,11 @@ class Sentence < ActiveRecord::Base
   private
   def compute_length
     length = content.split(' ').count
+  end
+
+  def self.invalid_ids_for(user)
+    list = user.tasks.map(&:sentence_id)
+
+    list.empty? ? [-1] : list
   end
 end
